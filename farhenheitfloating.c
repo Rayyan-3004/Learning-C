9169d3496501817f67327c09c3576b98914b7256
Rayyan Ur Rehman Massan <k252014@nu.edu.pk>
2025-09-09 08:32:05 +0500

    gpa new project

--- farhenheitfloating.c
+++ farhenheitfloating.c
@@ -14,7 +14,7 @@
      fahr = lower;
      while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%f %f\n", fahr, celsius);
         fahr = fahr + step;
      }
  } 
